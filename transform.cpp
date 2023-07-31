9a10776fd6d15b87a137926684a5ed6837d0e971
vietbach-tran <a1901793@adelaide.edu.au>
2023-07-31 10:39:07 +0930

    ws02

---
+++ transform.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int pow10(int n){
+  int ans=1;
+  for(int i=0;i<n;i++){
+    ans *= 10;
+  }
+  return ans;
+}
+
+int basetentotwo(int n){
+  int count = 0;
+  int ans = 0;
+  while(n>0){
+    ans = ans + (n%2)*pow10(count);
+    n = int(n/2);
+    count++;
+  }
+  return ans;
+}
+
+int main(){
+  std::cout << basetentotwo(12);
+  return 0;
+}
