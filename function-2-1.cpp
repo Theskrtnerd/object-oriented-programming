6461c5047cdd94fa573b7c93ecec7eb3f5061dcd
vietbach-tran <a1901793@adelaide.edu.au>
2023-08-02 10:51:14 +0930

    prac2

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 using namespace std;
+#include <cmath>
 
-int pow10(int n){
-  int ans=1;
+long long pow10(long long n){
+  long long ans=1;
   for(int i=0;i<n;i++){
     ans *= 10;
   }
   return ans;
 }
 
-int basetentotwo(int n){
+long long basetentotwo(long long n){
   int count = 0;
-  int ans = 0;
+  long long ans = 0;
   while(n>0){
     ans = ans + (n%2)*pow10(count);
-    n = int(n/2);
+    cout << ans << endl;
+    n = floor(n/2);
     count++;
   }
   return ans;
 }
 
 void print_binary_str(string decimal_number){
-  int number = stoi(decimal_number);
+  long long number = stoi(decimal_number);
   cout << number << endl;
-  int output_number = basetentotwo(number);
+  long long output_number = basetentotwo(number);
   cout << output_number;
 }
