2fa681ca05c0137153bf6ca1ecfea9fc784fac35
vietbach-tran <a1901793@adelaide.edu.au>
2023-08-02 11:27:58 +0930

    prac2

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -4,12 +4,29 @@ using namespace std;
 
 int sum_if_palindrome(int integer[], int length){
   int sum=0;
-  for(int i=0;i<length,i++){
+  for(int i=0;i<length;i++){
     sum+=integer[i];
   }
   return sum;
 }
 
 bool is_palindrome(int integer[], int length){
-  
+  for(int i=0; i<int((length-1)/2);i++){
+    if(integer[i]!=integer[length-1-i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+int sum_array_elements(int integer[], int length){
+  if(length < 1){
+    return -1;
+  }
+  if(is_palindrome(integer,length)){
+    return sum_if_palindrome(integer,length);
+  }
+  else{
+    return -2;
+  }
 }
--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 #include "function-2-3.cpp"
 
-extern void print_binary_str(string);
+extern int sum_array_elements(string);
 
 int main(){
-  int binary_digits[10] = {1,0,0,0,1,0,0,1,0,1};
-  cout << binary_to_int(binary_digits,10);
+  int binary_digits[10] = {1,0,0,0,1,1,0,0,0,1};
+  cout << sum_array_elements(binary_digits,10);
   return 0;
 }
