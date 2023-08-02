03cfe749848567046d8d284d5acfe4b9a974f0f3
vietbach-tran <a1901793@adelaide.edu.au>
2023-08-02 10:43:24 +0930

    prac2

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+
+int basetentotwo(int n){
+  int count = 0;
+  int ans = 0;
+  while(n>0){
+    ans = ans + (n%2)*pow10(count);
+    n = int(n/2);
+    count++;
+  }
+  return ans;
+}
+
 void print_binary_str(string decimal_number){
   int decimal_number = stoi(decimal_number);
 }
