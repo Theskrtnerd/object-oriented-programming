76a4459a20db4c3c281fdec84a67f374cf0b9413
vietbach-tran <a1901793@adelaide.edu.au>
2023-08-02 10:47:31 +0930

    prac2

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int pow10(int n){
+  int ans=1;
+  for(int i=0;i<n;i++){
+    ans *= 10;
+  }
+  return ans;
+}
 
 int basetentotwo(int n){
   int count = 0;
@@ -14,5 +21,8 @@ int basetentotwo(int n){
 }
 
 void print_binary_str(string decimal_number){
-  int decimal_number = stoi(decimal_number);
+  int number = stoi(decimal_number);
+  cout << number << endl;
+  int output_number = basetentotwo(number);
+  cout << output_number;
 }
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
-#include "function-1-5.cpp"
+#include "function-2-1.cpp"
 
-extern void print_summed(int[3][3],int[3][3]);
+extern void print_binary_str(string);
 
 int main(){
-  int matrix1[3][3] = {{0,1,2},{3,4,5},{6,7,8}} ;
-  int matrix2[3][3] = {{0,0,0},{2,2,2},{-5,-4,8}} ;
-  print_summed(matrix1,matrix2) ;
+  string decimal_number = "1234";
+  print_binary_str(decimal_number) ;
   return 0;
 }
