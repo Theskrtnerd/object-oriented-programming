2805cf343726f21ee9ab57735ca37b0bc2d16f37
vietbach-tran <a1901793@adelaide.edu.au>
2023-08-02 11:43:06 +0930

    prac2

--- main-2-4.cpp
+++ main-2-4.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
-#include "function-2-4.cpp"
 
-extern int sum_min_max(string);
+extern int sum_min_max(int[],int);
 
 int main(){
   int binary_digits[10] = {1,0,0,0,1,1,0,0,0,1};
-  cout << sum_min_max(binary_digits,10);
+  std::cout << sum_min_max(binary_digits,10);
   return 0;
 }
