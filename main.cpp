6dc033d509928280f57455c0e28931934ed6f99b
vietbach-tran <a1901793@adelaide.edu.au>
2023-08-08 11:57:09 +0930

    workshop3

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include "workshop.h"
+
+using namespace std;
+
+int main(){
+  // part 2
+  double* a = new double(3.0);
+  changeValue(a);
+  cout << *a;
+
+  // part 3
+
+  return 0;
+}
---
+++ workshop.h
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void changeValue(double* a){
+  *a = 42.0; 
+}
+
+void printArray(double* arr, int n){
+  for(int i=0; i<n;i++){
+    cout << arr[i] << endl;
+  }
+}
+
+double arrayMax(double* arr, int n){
+  double max = arr[0];
+  for(int i=1;i<n;i++){
+    if (arr[i] > max){
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+double* dynamicArray(int n, double m){
+  double* arr = new double[n];
+  for(int i=0; i<n;i++){
+    arr[i] = m;
+  }
+  return arr;
+}
