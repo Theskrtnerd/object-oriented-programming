c753cb15bb9e27844de62e4ce3de64c2910d6148
xineohperif <tvbbd2@gmail.com>
2023-08-24 15:22:33 +0930

    v1

--- House.cpp
+++ House.cpp
@@ -4,5 +4,23 @@
 using namespace std;
 
 House::House(){
-  
+  _ownerName = "";
+  _lotNumber = 0;
 }
+
+House::House(string ownerName, int lotNumber){
+  _ownerName = ownerName;
+  _lotNumber = lotNumber;
+}
+
+string House::get_owner_name(){
+  return _ownerName;
+};                // returns the name of the house owner
+
+int House::get_lot_number(){
+  return _lotNumber;
+};                        // returns the house's lot number
+
+House::~House(){
+  cout << "House removed";
+};
--- House.h
+++ House.h
@@ -1,13 +1,18 @@
+#ifndef HOUSE_H
+#define HOUSE_H
+
 #include<string>
 
 class House{
   private:
-    std::string ownerName;
-    int lotNumber;
+    std::string _ownerName;
+    int _lotNumber;
   public:
     House();                                     // a default constructor lot number is zero and owner name is an empty string ""
     House(std::string ownerName, int lotNumber); // a constructor that takes the lot number and house owner name
     std::string get_owner_name();                // returns the name of the house owner
     int get_lot_number();                        // returns the house's lot number
     ~House();
-} 
+};
+
+#endif
