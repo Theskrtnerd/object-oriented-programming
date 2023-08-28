e3511b289494ad292644ee38c3cff0c7c84de26f
xineohperif <tvbbd2@gmail.com>
2023-08-28 22:58:42 +0930

    prac4

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -14,7 +14,7 @@ int main(){
     cin >> initial;
     switch(initial){
       case 'C':
-        lot.parkVehicle(1);
+        lot.parkVehicle(Car(i+1));
         break;
       case 'B':
         lot.parkVehicle(Bus(i+1));
