b9007267802c1d3734f23968588c994af6e39c0c
Theskrtnerd <tvbbd2@gmail.com>
2023-09-04 17:47:35 +0930

    1-1

--- Appliance.cpp
+++ Appliance.cpp
@@ -13,6 +13,22 @@ Appliance::Appliance(int powerRating){
   this->isOn = false;
 }
 
+int Appliance::get_powerRating(){
+  return this->powerRating;
+}
+
+bool Appliance::get_isOn(){
+  return this->isOn;
+}
+
+void Appliance::set_powerRating(int powerRating){
+  this->powerRating = powerRating;
+}
+
+void Appliance::set_isOn(bool isOn){
+  this->isOn = isOn;
+}
+
 void Appliance::turnOn(){
   this->isOn = true;
 }
--- Appliance.h
+++ Appliance.h
@@ -9,6 +9,12 @@ class Appliance{
     Appliance();
     Appliance(int powerRating);
 
+    int get_powerRating();
+    bool get_isOn();
+    void set_powerRating(int powerRating);
+    void set_isOn(bool isOn);
+
+
     void turnOn();
     void turnOff();
     virtual double getPowerConsumption();
