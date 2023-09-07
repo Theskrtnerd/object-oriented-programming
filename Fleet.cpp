9760d3fc65ba37f1296cacdfa28fe52510e3df6f
Theskrtnerd <tvbbd2@gmail.com>
2023-09-07 16:59:06 +0930

    pe3

--- Fleet.cpp
+++ Fleet.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #include "Ford.h"
 
 Fleet::Fleet(){
-  this->fleet = new Car*[5];
+  *this->fleet = new Car[5];
   this->fleet[0] = new Tesla('S',10000);
   this->fleet[1] = new Ford(1,20000);
   this->fleet[2] = new Car(30000);
@@ -18,6 +18,9 @@ Car** Fleet::get_fleet(){
 }
 
 Fleet::~Fleet(){
+  for(int i=0;i<5;i++){
+    delete this->fleet[i];
+  }
   delete[] this->fleet;
 }
 
