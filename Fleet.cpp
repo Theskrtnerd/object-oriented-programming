0e9061570e1df9d5075e15b9272a42e6f0593326
Theskrtnerd <tvbbd2@gmail.com>
2023-09-07 16:34:23 +0930

    pe3

--- Fleet.cpp
+++ Fleet.cpp
@@ -11,14 +11,9 @@ Fleet::Fleet(){
   this->fleet[2] = new Car(30000);
   this->fleet[3] = new Ford(2, 40000);
   this->fleet[4] = new Tesla('T',50000);
-
 }
 
 Car** Fleet::get_fleet(){
   return this->fleet;
-}
-
-Fleet::~Fleet(){
-  delete[] this->fleet;
 }
 
