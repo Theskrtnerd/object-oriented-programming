7e36b9e0adb49b3a5327c0cf81d76ceaf6069931
Theskrtnerd <tvbbd2@gmail.com>
2023-10-20 05:24:31 +1030

    ws12

--- Utils.h
+++ Utils.h
@@ -8,7 +8,10 @@
 class Utils{
     public:
         Utils(){};
-        std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight);
+        std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight){
+            std::tuple tuple(std::rand() % (gridWidth+1), std::rand() % (gridHeight+1));
+            return tuple;
+        };
         double calculateDistance(std::tuple<int, int> pos1, std::tuple<int, int> pos2){
             return std::sqrt((std::get<0>(pos1) - std::get<0>(pos2))*(std::get<0>(pos1) - std::get<0>(pos2)) 
                 + (std::get<1>(pos1) - std::get<1>(pos2))*(std::get<1>(pos1) - std::get<1>(pos2)));
