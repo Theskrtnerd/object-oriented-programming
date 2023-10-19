fab7f77b5aa2055a3f12e17ffb5aa0a2758ce8f1
Theskrtnerd <tvbbd2@gmail.com>
2023-10-20 05:55:33 +1030

    ws12

--- Ship.h
+++ Ship.h
@@ -8,7 +8,8 @@ class Ship: public GameEntity{
     public:
         Ship(int x, int y): GameEntity(x, y, 'S'){};
         void move(int dx, int dy){
-            this->setPosition(dx, dy);
+            std::tuple<int, int> position_ = this->getPos();
+            this->setPosition(std::get<0>(position_)+dx, std::get<1>(position_)+dy);
         }
         ~Ship(){};
 };
