b97a326a541c0a466a9bfc9a4a148dbf8ab719ba
Theskrtnerd <tvbbd2@gmail.com>
2023-10-26 16:29:29 +1030

    pe4

--- Cell.h
+++ Cell.h
@@ -6,17 +6,17 @@
 using namespace std;
 
 class Cell {
- protected:
-  std::tuple<int, int> position;
-  char type;
+    protected:
+        std::tuple<int, int> position;
+        char type;
 
- public:
-  Cell(int x, int y, char type) : position(make_tuple(x, y)), type(type) {}
-  std::tuple<int, int> getPos() { return position; }
-  char getType() { return type; }
-  void setPos(int x, int y) { position = make_tuple(x, y); }
-  void setType(char type) { this->type = type; }
-  virtual ~Cell() {}
+    public:
+        Cell(int x, int y, char type) : position(make_tuple(x, y)), type(type) {}
+        std::tuple<int, int> getPos() { return position; }
+        char getType() { return type; }
+        void setPos(int x, int y) { position = make_tuple(x, y); }
+        void setType(char type) { this->type = type; }
+        virtual ~Cell() {}
 };
 
 #endif
--- Game.h
+++ Game.h
@@ -13,74 +13,76 @@ using namespace std;
 #include "Utils.h"
 
 class Game {
- private:
-  std::vector<Cell*> grid;
-  int width;
-  int height;
+    private:
+        std::vector<Cell*> grid;
+        int width;
+        int height;
 
- public:
-  Game() {}
-  std::vector<Cell*>& getGrid() { return grid; }
-  void setGrid(vector<Cell*> newGrid) { grid = newGrid; }
-  void initGame(int numCharacters, int numTraps, int gridWidth,
-                int gridHeight) {
-    width = gridWidth;
-    height = gridHeight;
-    vector<Cell*> newGrid;
-    for (int i = 0; i < numCharacters; i++) {
-      tuple<int, int> pos = Utils::generateRandomPos(gridWidth, gridHeight);
-      int x = get<0>(pos);
-      int y = get<1>(pos);
-      newGrid.push_back(new Character(x, y));
+    public:
+    Game() {}
+    std::vector<Cell*>& getGrid(){
+        return grid;
     }
-    for (int i = 0; i < numTraps; i++) {
-      tuple<int, int> pos = Utils::generateRandomPos(gridWidth, gridHeight);
-      int x = get<0>(pos);
-      int y = get<1>(pos);
-      newGrid.push_back(new Trap(x, y));
+    void setGrid(vector<Cell*> newGrid){
+        grid = newGrid;
     }
-    setGrid(newGrid);
-  }
-  void gameLoop(int maxIterations, double trapActivationDistance) {
-    bool winCon = false;
-    int iter = 0;
-    while (iter < maxIterations) {
-      for (Cell* cell : grid) {
-        if (dynamic_cast<Character*>(cell)) {
-          Character* character = dynamic_cast<Character*>(cell);
-          character->move(1, 0);
-          int x = get<0>(character->getPos());
-          int y = get<1>(character->getPos());
-          if (x > width || y > height) {
-            winCon = true;
-            goto end;
-          }
+    void initGame(int numCharacters, int numTraps, int gridWidth, int gridHeight){
+        width = gridWidth;
+        height = gridHeight;
+        vector<Cell*> newGrid;
+        for (int i = 0; i < numCharacters; i++) {
+            tuple<int, int> pos = Utils::generateRandomPos(gridWidth, gridHeight);
+            int x = get<0>(pos);
+            int y = get<1>(pos);
+            newGrid.push_back(new Character(x, y));
         }
-        if (dynamic_cast<Trap*>(cell)) {
-          Trap* trap = dynamic_cast<Trap*>(cell);
-          for (Cell* character : grid) {
-            if (dynamic_cast<Character*>(character)) {
-              Character* character = dynamic_cast<Character*>(character);
-              double dist =
-                  Utils::calculateDistance(trap->getPos(), character->getPos());
-              if (dist <= trapActivationDistance) {
-                trap->apply(*character);
-              }
+        for (int i = 0; i < numTraps; i++) {
+            tuple<int, int> pos = Utils::generateRandomPos(gridWidth, gridHeight);
+            int x = get<0>(pos);
+            int y = get<1>(pos);
+            newGrid.push_back(new Trap(x, y));
+        }
+        setGrid(newGrid);
+    }
+    void gameLoop(int maxIterations, double trapActivationDistance) {
+        bool hasWon = false;
+        int iter = 0;
+        while (iter < maxIterations) {
+        for (Cell* cell : grid) {
+            if (dynamic_cast<Character*>(cell)) {
+            Character* character = dynamic_cast<Character*>(cell);
+            character->move(1, 0);
+            int x = get<0>(character->getPos());
+            int y = get<1>(character->getPos());
+            if (x > width or y > height) {
+                hasWon = true;
+                goto end;
             }
-          }
+            }
+            if (dynamic_cast<Trap*>(cell)) {
+            Trap* trap = dynamic_cast<Trap*>(cell);
+            for (Cell* character : grid) {
+                if (dynamic_cast<Character*>(character)) {
+                Character* character = dynamic_cast<Character*>(character);
+                double dist = Utils::calculateDistance(trap->getPos(), character->getPos());
+                if (dist <= trapActivationDistance) {
+                    trap->apply(*character);
+                }
+                }
+            }
+            }
         }
-      }
-      iter++;
-    }
-    if (iter >= maxIterations) {
-      cout << "Maximum number of iterations reached. Game over." << endl;
-    }
+        iter++;
+        }
+        if (iter >= maxIterations) {
+        cout << "Maximum number of iterations reached. Game over." << endl;
+        }
 
-  end:
-    if (winCon == true) {
-      cout << "Character has won the game!" << endl;
+    end:
+        if (hasWon == true) {
+        cout << "Character has won the game!" << endl;
+        }
     }
-  }
-};
+    };
 
 #endif
