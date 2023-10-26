f036f2b23dd346dc192897e970277056798973a1
Theskrtnerd <tvbbd2@gmail.com>
2023-10-26 15:50:53 +1030

    pe4-q3

---
+++ Game.h
@@ -0,0 +1,81 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <vector>
+#include "Cell.h"
+#include "Trap.h"
+#include "Utils.h"
+#include "Character.h"
+#include <iostream>
+
+class Game{
+    private:
+        std::vector<Cell*> grid;
+        std::vector<Character> players;
+        std::vector<Trap> traps;
+        int gridWidth;
+
+    public:
+        Game(){}
+        std::vector<Cell*>& getGrid(){
+            return this->grid;
+        }
+
+        void initGame(int numCharacters, int numTraps, int gridWidth, int gridHeight){
+            this->gridWidth = gridWidth;
+            grid.resize(numCharacters+numTraps);
+            players.resize(numCharacters);
+            traps.resize(numTraps);
+
+            for(int i=0;i<numCharacters;i++){
+                std::tuple<int, int> pos_ = Utils::generateRandomPos(gridWidth, gridHeight);
+                int x_ = std::get<0>(pos_);
+                int y_ = std::get<1>(pos_);
+                Character player(x_, y_);
+                players[i] = player;
+                Character* player_ptr = &player;
+                grid[i] = player_ptr;
+            }
+
+            for(int i=0;i<numTraps;i++){
+                std::tuple<int, int> pos_ = Utils::generateRandomPos(gridWidth, gridHeight);
+                int x_ = std::get<0>(pos_);
+                int y_ = std::get<1>(pos_);
+                Trap trap(x_, y_);
+                traps[i] = trap;
+                Trap* trap_ptr = &trap;
+                grid[numCharacters+i] = trap_ptr;
+            }
+
+        }
+
+        void gameLoop(int maxIterations, double trapActivationDistance){
+            int iter = 0;
+            while(iter < maxIterations){
+                for(int i=0;i<players.size();i++){
+                    players[i].move(1,0);
+                }
+                for(int i=0;i<players.size();i++){
+                    for(int j=0;j<traps.size();j++){
+                        if(Utils::calculateDistance(traps[j].getPos(), players[i].getPos()) < trapActivationDistance){
+                            traps[j].apply(players[i]);
+                        }
+                    }
+                }
+
+                for(int i=0;i<players.size();i++){
+                    int player_pos = std::get<0>(players[i].getPos());
+                    if(player_pos > this->gridWidth && players[i].getType() == 'C'){
+                        "Character has won the game!";
+                        return;
+                    }
+                }
+
+                iter++;
+            }
+            std::cout << "Maximum number of iterations reached. Game over." << std::endl;
+            return;
+        }
+};
+
+#endif
---
+++ main-3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+#include "Cell.h"
+#include "Utils.h"
+#include "Trap.h"
+#include "Character.h"
+#include "Game.h"
+
+int main(){
+    Game game;
+    Cell cell1(1,3,'C');
+    Cell cell2(4,7,'C');
+    Trap trap(1,9);
+    Character user(1,8);
+    cout << user.getType() << endl;
+    cout << std::get<0>(user.getPos()) << ", " << std::get<1>(user.getPos()) << endl;
+    user.move(0,1);
+    cout << std::get<0>(user.getPos()) << ", " << std::get<1>(user.getPos()) << endl;
+    trap.apply(user);
+    cout << user.getType() << endl;
+    
+    cout << Utils::calculateDistance(cell1.getPos(), cell2.getPos()) << endl;
+
+
+    cout << "Game starts" << endl;
+    cout << "------------------" << endl;
+
+    game.initGame(2, 8, 3, 4);
+    game.gameLoop(10, 2);
+
+    cout << "------------------" << endl;
+    cout << "Game ended!" << endl;
+
+}
