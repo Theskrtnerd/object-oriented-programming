1f77a12831d1c27474fa38811c27505a5921a9db
Theskrtnerd <tvbbd2@gmail.com>
2023-10-26 15:56:55 +1030

    pe4-q3

--- Utils.h
+++ Utils.h
@@ -4,19 +4,21 @@
 #include <tuple>
 #include <cmath>
 
+using namespace std;
+
 class Utils{
     public:
         Utils(){};
-        static std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight){
+        static tuple<int, int> generateRandomPos(int gridWidth, int gridHeight){
             int x = rand() % gridWidth+1;
             int y = rand() % gridHeight+1;
-            std::tuple<int, int> newPos = std::make_tuple(x, y);
+            tuple<int, int> newPos = make_tuple(x, y);
             return newPos;
         }
-        static double calculateDistance(std::tuple<int, int> pos1, std::tuple<int, int> pos2){
-            int a = (std::get<0>(pos1) - std::get<0>(pos2))*(std::get<0>(pos1) - std::get<0>(pos2))
-             + (std::get<1>(pos1) - std::get<1>(pos2))*(std::get<1>(pos1) - std::get<1>(pos2));
-            return std::sqrt(a);
+        static double calculateDistance(tuple<int, int> pos1, tuple<int, int> pos2){
+            int a = (get<0>(pos1) - get<0>(pos2))*(get<0>(pos1) - get<0>(pos2))
+             + (get<1>(pos1) - get<1>(pos2))*(get<1>(pos1) - get<1>(pos2));
+            return sqrt(a);
         }
 };
 
