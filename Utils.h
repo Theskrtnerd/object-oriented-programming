f31d528196e1be97ee6cca9ffe898fb77cf9a02c
Theskrtnerd <tvbbd2@gmail.com>
2023-10-26 15:16:57 +1030

    pe4

--- Utils.h
+++ Utils.h
@@ -8,7 +8,10 @@ class Utils{
     public:
         Utils(){};
         static std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight){
-
+            int x = rand() % gridWidth+1;
+            int y = rand() % gridHeight+1;
+            tuple<int, int> newPos = make_tuple(x, y);
+            return newPos;
         }
         static double calculateDistance(std::tuple<int, int> pos1, std::tuple<int, int> pos2){
             int a = (std::get<0>(pos1) - std::get<0>(pos2))*(std::get<0>(pos1) - std::get<0>(pos2))
